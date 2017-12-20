9658fc13ec35ca09bed68e718836e5b6ff433247
JansenME <martijn.e.jansen@rabobank.nl>
2017-12-20 17:49:33 +0100

    First commit

---
+++ src/main/java/com/competition/CompetitionRabobankApplication.java
@@ -0,0 +1,12 @@
+package com.competition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitionRabobankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetitionRabobankApplication.class, args);
+	}
+}
